32fda85f472438ce25d1c3336fdefc4bcc601289
RiversCoder <928091232@qq.com>
2018-05-01 16:03:38 +0800

    new

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1525161455,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/p-sheet0.png",
+		"images/plane-sheet0.png",
+		"images/master-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/墙-sheet0.png",
+		"images/letter_cover-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
